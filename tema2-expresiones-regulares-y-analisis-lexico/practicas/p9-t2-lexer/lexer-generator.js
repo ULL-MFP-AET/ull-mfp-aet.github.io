bf1934ffdacc7c953ca2262891d2e39aea6cd38b
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-03-20 16:47:50 +0000

    lexer-generator.js-is-a-lexer-generator

---
+++ tema2-expresiones-regulares-y-analisis-lexico/practicas/p9-t2-lexer/lexer-generator.js
@@ -0,0 +1,33 @@
+function buildLexer(lexemes) {
+  let tokens = lexemes.slice(0);
+
+  const buildOrRegexp = (regexps) => {
+    const sources = regexps.map(r => r.source);
+    const union = sources.join('|');
+    //console.log(union);
+    return new RegExp(union, 'y');
+  };
+
+  tokens.push(['ERROR', /(?<ERROR>(.|\n)+)/]);
+  const tokenNames = tokens.map(t => t[0]);
+  const tokenRegs  = tokens.map(t => t[1]);
+  const regexp = buildOrRegexp(tokenRegs);
+  //console.log(regexp);
+  const getToken = (m) => tokenNames.find(tn => typeof m[tn] !== 'undefined');
+
+  return str => {
+    let result = [];
+    let match;
+    while (match = regexp.exec(str)) {
+      //console.log(match.groups);
+      let t = getToken(match.groups);
+      //if (t === 'ERROR') throw `Unxpected token! '${str.substring(regexp.lastIndex)}'`;
+      //console.log(`Found token '${t}' with value '${match.groups[t]}'`)
+      if (t !== 'SPACE')
+        result.push({ type: t, value: match.groups[t]});
+    }
+    return result;
+  }
+}
+
+module.exports = buildLexer;
