3dc57dd16c74a08288046997a79dc91fdc8cddbc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-03-20 16:39:30 +0000

    sticky.js-is-a-lexer-generator

--- tema2-expresiones-regulares-y-analisis-lexico/practicas/p9-t2-lexer/sticky.js
+++ tema2-expresiones-regulares-y-analisis-lexico/practicas/p9-t2-lexer/sticky.js
@@ -8,7 +8,7 @@ function buildLexer(lexemes) {
     return new RegExp(union, 'y');
   };
 
-  tokens.push(['ERROR', /(?<ERROR>.|\n)/]);
+  tokens.push(['ERROR', /(?<ERROR>(.|\n)+)/]);
   const tokenNames = tokens.map(t => t[0]);
   const tokenRegs  = tokens.map(t => t[1]);
   const regexp = buildOrRegexp(tokenRegs);
@@ -21,9 +21,10 @@ function buildLexer(lexemes) {
     while (match = regexp.exec(str)) {
       //console.log(match.groups);
       let t = getToken(match.groups);
-      if (t === 'ERROR') throw `Unxpected token! '${str.substring(regexp.lastIndex)}'`;
+      //if (t === 'ERROR') throw `Unxpected token! '${str.substring(regexp.lastIndex)}'`;
       //console.log(`Found token '${t}' with value '${match.groups[t]}'`)
-      result.push({ type: t, value: match.groups[t]});
+      if (t !== 'SPACE')
+        result.push({ type: t, value: match.groups[t]});
     }
     return result;
   }
@@ -42,20 +43,23 @@ const myTokens = [
   ['STRING', STRING], ['OP', OP] 
 ];
 
+const { inspect } = require('util');
+const ins = (x) => console.log(inspect(x, {depth: null}));
 let str, lexer, r;
 lexer = buildLexer(myTokens);
 
 str = 'const varName = "value"';
 console.log(str);
 r = lexer(str);
-r.forEach((t, i) => {
-  console.log(`Found token '${t.type}' with value '${t.value}'`);
-});
+ins(r);
 
-lexer = buildLexer(myTokens);
 str = 'let x = a + b';
 console.log(str);
 r = lexer(str);
-r.forEach((t, i) => {
-  console.log(`Found token '${t.type}' with value '${t.value}'`);
-});
+ins(r);
+
+// Entrada con errores
+str = 'let x = 42*c';
+console.log(str);
+r = lexer(str);
+ins(r);
